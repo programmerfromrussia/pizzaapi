49f6b4fb27a8ca5d1a36d43667551611d57f1df3
programmerfromrussia <programmerfromrussia@gmail.com>
2025-01-21 01:13:52 +0300

    little changes in Authorisation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        $validated = Validator::make($request->all(),[
+    public function register(Request $request)
+    {
+        $validated = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
 
-        if($validated->fails()){
-            return response()->json($validated->errors(), status: 422);
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 422);
         }
 
         try {
             $user = User::create([
                 'name' => $request->name,
-                'email' =>  $request->email,
+                'email' => $request->email,
                 'password' => Hash::make($request->password),
             ]);
 
@@ -32,12 +33,11 @@ class AuthController extends Controller
             return response()->json([
                 'access_token' => $token,
                 'user' => $user
-            ], status: 200);
+            ], 200);
         } catch (\Exception $exception) {
             return response()->json([
-                'error' => $exception->getMessage()
-            ], status: 403);
+                'error' => 'Something went wrong. Please try again later.'
+            ], 500);
         }
-
     }
 }
